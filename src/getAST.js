054c80e2a677dac7cab85a55c8b50964ccaff44c
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2020-03-04 00:40:30 +0300

    Fix and reduce getAST logic

--- src/getAST.js
+++ src/getAST.js
@@ -1,60 +1,51 @@
-/* eslint-disable no-param-reassign */
+import { has, union } from 'lodash';
 
-import { has, reduce } from 'lodash';
+const isTypeTypesBothEqualAndObject = (firstType, secondType) => firstType === secondType && firstType === 'object';
+const areValuesEqual = (firstValue, secondValue) => firstValue === secondValue;
 
 const getAST = (firstObj, secondObj) => {
-  const mergedObj = {
-    ...firstObj,
-    ...secondObj,
-  };
-  const result = reduce(
-    mergedObj,
-    (acc, value, key) => {
-      if (has(firstObj, key) && has(secondObj, key)) {
-        if (typeof value === typeof firstObj[key] && typeof value === 'object') {
-          acc.push({
-            key,
-            state: 'same',
-            value: getAST(firstObj[key], value),
-          });
-          return acc;
-        }
-        if (typeof value !== 'object' && typeof firstObj[key] !== 'object') {
-          if (value === firstObj[key]) {
-            acc.push({
-              key,
-              state: 'same',
-              value,
-            });
-            return acc;
-          }
-        }
-        acc.push({
-          key,
-          state: 'changed',
-          beforeValue: firstObj[key],
-          afterValue: value,
-        });
-        return acc;
-      }
-      if (has(secondObj, key)) {
-        acc.push({
-          key,
-          state: 'added',
-          value,
-        });
-        return acc;
-      }
-      acc.push({
-        key,
-        state: 'deleted',
-        value,
-      });
-      return acc;
-    },
-    [],
+  const mergedKeys = union(
+    Object.keys(firstObj),
+    Object.keys(secondObj),
   );
-  return result;
+  const ast = mergedKeys.map((key) => {
+    const isFirstObjHasKey = has(firstObj, key);
+    const isSecondObjHasKey = has(secondObj, key);
+
+    if (isFirstObjHasKey && isSecondObjHasKey) {
+      const firstValue = firstObj[key];
+      const secondValue = secondObj[key];
+      const firstValueType = typeof firstValue;
+      const secondValueType = typeof secondValue;
+
+      if (isTypeTypesBothEqualAndObject(firstValueType, secondValueType)) {
+        return {
+          key,
+          state: 'same',
+          value: getAST(firstValue, secondValue),
+        };
+      }
+      if (areValuesEqual(firstValue, secondValue)) {
+        return {
+          key,
+          state: 'same',
+          value: firstValue,
+        };
+      }
+      return {
+        key,
+        state: 'changed',
+        beforeValue: firstValue,
+        afterValue: secondValue,
+      };
+    }
+    return {
+      key,
+      state: isSecondObjHasKey ? 'added' : 'deleted',
+      value: isSecondObjHasKey ? secondObj[key] : firstObj[key],
+    };
+  });
+  return ast;
 };
 
 export default getAST;
